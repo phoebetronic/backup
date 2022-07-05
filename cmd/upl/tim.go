998bfcfaa21a2832db9791b973af1f6fe81d1db3
Tim Schindler <xh3b4sd@gmail.com>
2022-07-06 01:12:24 +0200

    fix

--- cmd/upl/tim.go
+++ cmd/upl/tim.go
@@ -6,11 +6,6 @@ func bacfmt(tim time.Time) string {
 	return tim.UTC().Format("06-01-02")
 }
 
-func monfmt(tim time.Time) time.Time {
-	y, m, _ := tim.Date()
-	return time.Date(y, m, 1, 0, 0, 0, 0, tim.UTC().Location())
-}
-
 func scrfmt(tim time.Time) string {
 	return tim.UTC().Format("06-01-02 15:04:05")
 }
