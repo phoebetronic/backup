c321b31631705542bf150e724419d3fe207e36fe
xh3b4sd <xh3b4sd@gmail.com>
2022-07-16 19:54:07 +0200

    fix

--- cmd/win/cmd.go
+++ cmd/win/cmd.go
@@ -7,12 +7,17 @@ import (
 const (
 	use = "win"
 	sho = "Generate window frames of training input data."
-	lon = `
-Generate window frames of training input data.
+	lon = `Generate window frames of training input data. In order to write the generated
+training data into a .csv file use the command below.
 
     backup win --tim 22-06-01 --csv
 
-    backup win --tim 22-06-01 --dra
+Further, graphs of the generated training data can be plotted by selecting a
+window frame index determining which window frames to plot. Note that 10 graphs
+will be plotted at a time, which will all be very similar due to the sequential
+nature of the generated window frames.
+
+    backup win --tim 22-06-01 --dra --ind 20
 `
 )
 
