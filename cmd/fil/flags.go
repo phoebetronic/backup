5f2f46a4de13473c450f81c7b6d3b6d5507ebb33
xh3b4sd <xh3b4sd@gmail.com>
2022-09-19 20:31:15 +0200

    Update cmd/fil/flags.go

--- cmd/fil/flags.go
+++ cmd/fil/flags.go
@@ -24,8 +24,8 @@ func (f *flags) Create(cmd *cobra.Command) {
 	cmd.Flags().DurationVarP(&f.Dur, "dur", "", 0, "The frame duration of buffered trades, e.g. 1m.")
 	cmd.Flags().StringVarP(&f.Kin, "kin", "k", "", "The kind of backup data to validate, e.g. ord, tra.")
 	cmd.Flags().StringVarP(&f.Pat, "pat", "", "", "The file path to write trades to, e.g. /Users/xh3b4sd/phoebetron/001.json.")
-	cmd.Flags().StringVarP(&f.sta, "sta", "", "", "Time string for backup data start date in form of yy-mm-ddThh:mm:ss.")
-	cmd.Flags().StringVarP(&f.end, "end", "", "", "Time string for backup data end date in form of yy-mm-ddThh:mm:ss.")
+	cmd.Flags().StringVarP(&f.sta, "sta", "", "", "Time string for backup data start date in form of yy-mm-ddThh:00:00.")
+	cmd.Flags().StringVarP(&f.end, "end", "", "", "Time string for backup data end date in form of yy-mm-ddThh:00:00.")
 }
 
 func (f *flags) Verify() {
